9a942679ecfe3fb5e8a77af927ce07e53891b008
withtaylors <withtaylors@naver.com>
2023-05-30 20:32:57 +0900

    1차 대수정

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9996b7a1c6a46a025c9f85ecf51ac4dfa751701049fb8bf3c682ac9419fea1f6
-size 2913
+oid sha256:16b6040f5e84bc92fd26f7dcd0e934d962ac2f9b06213e480c990b50caaf53e1
+size 2198
