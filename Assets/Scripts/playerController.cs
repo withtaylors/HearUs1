55c30804f118ff89447cc2f03e705db07e39a5e3
withtaylors <withtaylors@naver.com>
2023-06-01 20:46:12 +0900

    2차 복구 ver - 06.01
    다행히 fbx는 되돌아왔지만 전체적으로 다시 짜야함..

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c25747aadd3f820c47c3bb33a6ad1d2ccca47e2eaff7f985fcbbe9ad39102dd1
-size 3507
+oid sha256:e98f9220f3ec18a6ba6290e6e9c34d1e845ac8a6c0a8e0004f64fe35e0eae1f4
+size 3223
