19a09a3c7ddfcafd596be1b1803e49ab9d1cfc64
withtaylors <withtaylors@naver.com>
2023-06-03 00:29:06 +0900

    아이템 수정

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:931827b05d2c06765becfaf89a14adc27f61484916410dcabc129df08317ff45
-size 5074
+oid sha256:da9855d84d70a0494f421b3e96890bcacf59559a6cea66ba10d070676b487be2
+size 5341
