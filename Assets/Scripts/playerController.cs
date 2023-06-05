5dbedc3c5550f61bc5f2fa1470025dd75b24aec2
withtaylors <withtaylors@naver.com>
2023-06-06 01:11:36 +0900

    플레이어 애니메이션 등 vine 설정

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da9855d84d70a0494f421b3e96890bcacf59559a6cea66ba10d070676b487be2
-size 5341
+oid sha256:a585323d2f752e57bbf727b4d1b02b3233634c29d20bef5f1d1bc300b45dd36d
+size 6146
