f2d91c21741706b4b290925f409bf8eb3398862e
jiwon <knowledgeis1ne@gmail.com>
2023-06-08 00:46:45 +0900

    아이템 데이터베이스 추가
    망가진 태엽 인형, 버섯

--- Assets/Scripts/Inventory_MainGame/ItemDatabase.cs
+++ Assets/Scripts/Inventory_MainGame/ItemDatabase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e82fe8df2717e569a135095f6084de21b425cf4b7ef21a32bde5761c11cf9a01
-size 982
+oid sha256:556e0c517b96bc71bf177b18b0b319d30ed7dd0638a3187614b1883d112d79df
+size 1500
