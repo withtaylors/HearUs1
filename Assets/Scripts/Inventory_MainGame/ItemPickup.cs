811a38a51fd9cfc6d22e3320dfbb544e7c55176a
jiwon <knowledgeis1ne@gmail.com>
2023-06-08 15:03:21 +0900

    23-06-08 인벤토리에 아이템이 제대로 들어가지 않는 문제 해결

--- Assets/Scripts/Inventory_MainGame/ItemPickup.cs
+++ Assets/Scripts/Inventory_MainGame/ItemPickup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f9ed290770b483cfc8e362b9bb16ea7e754aabe27c1d7992bc84aa9cc4a2b5f
-size 1072
+oid sha256:815cecaa37f2d58cfc3e2e8ef07d4e78ddabbfab154fdd51deedf3764ad2e695
+size 551
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1996a57266fd9ea1ade0c2c6e8f07467947a34460ff9cb4237e054e5d525833c
-size 6233
+oid sha256:5d05ce1445e5416095ab9dca6b994eef559bde642071e6c7bf83e06539b31ddb
+size 6317
